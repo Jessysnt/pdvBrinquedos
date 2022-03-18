db609a1cdada7ff83dddc4486ef58f55bedd4b6b
Jessysnt <jadsantos92@gmail.com>
2022-03-17 22:28:35 -0300

    Inicializado construçao de comandas

--- classes/comandas.php
+++ classes/comandas.php
@@ -1,7 +1,22 @@
 <?php
 
 class comandas{
+	public function obterDadosC($idcliente){
+		$c= new conectar();
+		$conexao=$c->conexao();
 
+		$sql="SELECT id_cliente, nome, sobrenome from clientes where id_cliente = '$idcliente'"; 
+		
+		$result=mysqli_query($conexao,$sql);
+
+		$ver=mysqli_fetch_row($result);
+
+		$dados=array(
+			'nome' => $ver[1],
+			'sobrenome' => $ver[2]
+		);		
+		return $dados;
+	}
 }
 
 ?>
