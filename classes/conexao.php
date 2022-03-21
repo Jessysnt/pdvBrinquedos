db60155b2a380d478ef041fc8fcfa98bf6294f3b
Jessysnt <jadsantos92@gmail.com>
2022-03-21 19:27:18 -0300

    Tentativa de migraçao para pdo

--- classes/conexao.php
+++ classes/conexao.php
@@ -1,6 +1,22 @@
 <?php  
 
 class conectar{
+
+	/*private $pdo;
+
+	public function conexao(){
+		try {
+		$pdo = new PDO("mysql:dbname=lojab; host=localhost", "root", "");
+		} catch (PDOException $e) {
+			echo "Erro com banco de dados: ".$e->getMessage();
+		}
+		
+		catch(Exception $e){
+			echo "Erro generico: ".$e->getMessage();
+		}
+	
+	}*/
+	
 	private $servidor = "localhost";
 	private $usuario = "root";
 	private $senha = "";
@@ -10,8 +26,10 @@ class conectar{
 		$conexao = mysqli_connect($this->servidor, $this->usuario, $this->senha, $this->bd);
 
 		return $conexao;
-	}
+	} 
 }
+
+
 
 ?>
 
