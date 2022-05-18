584f74ef68212b690f9c9164ae3dd9877162f4d3
Jessysnt <jadsantos92@gmail.com>
2022-05-17 23:52:46 -0300

    Colocado ano

--- App/Repository/PainelDAO.php
+++ App/Repository/PainelDAO.php
@@ -9,7 +9,7 @@ class PainelDAO extends Conexao
 {
     public function vendasMesAno()
     {
-        $stmt = static::getConexao()->query("SELECT data_finalizacao FROM comandafatura WHERE data_finalizacao IS NOT null");
+        $stmt = static::getConexao()->query("SELECT DATE_FORMAT(data_finalizacao, '%Y') as ANO FROM comandafatura WHERE data_finalizacao is NOT null GROUP BY ANO");
         // $stmt->bindValue(':busca', '%'.$busca.'%');
         // $stmt->execute();
         // $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
