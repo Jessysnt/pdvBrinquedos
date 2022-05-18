d52260c4c6638a699388c2b66b8a1837e1190285
Jessysnt <jadsantos92@gmail.com>
2022-05-17 23:34:57 -0300

    Tras o ano

--- App/Repository/PainelDAO.php
+++ App/Repository/PainelDAO.php
@@ -12,6 +12,8 @@ class PainelDAO extends Conexao
         $stmt = static::getConexao()->query("SELECT data_finalizacao FROM comandafatura WHERE data_finalizacao IS NOT null");
         // $stmt->bindValue(':busca', '%'.$busca.'%');
         // $stmt->execute();
+        // $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        // die(var_dump($result));
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
